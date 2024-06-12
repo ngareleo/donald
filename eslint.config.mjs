f2918a8e0ad6ba0cac01c8ef29838a157ac66fd8
Titus Mumo <tituskennedy74@gmail.com>
2024-06-12 15:57:55 +0000

    modify eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,29 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import tsParser from "@typescript-eslint/parser";
 
 
 export default [
   {languageOptions: { globals: globals.node }},
+
   pluginJs.configs.recommended,
+
   ...tseslint.configs.recommended,
+
+  {
+    files: ["**/*.ts", "**/*.tsx"], 
+    languageOptions: {
+      parser: tsParser, 
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {
+      // "no-console": "warn",
+      "quotes": ["warning", "double"],
+      "semi": ["error", "always"],
+    },
+  },
+
 ];
